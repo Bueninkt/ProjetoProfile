ccecfc16a8f00748f85505549bad73c7ddac2c9b
MATHEUS BUENO MOREIRA DOS SANTOS <24122472@dsn127m1249386.cfp127aluno.local>
2025-05-08 16:42:38 -0300

    Tela-De-Perfil

--- app/src/main/java/br/senai/sp/jandira/projetoprofile/screen/ProfileScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/projetoprofile/screen/ProfileScreen.kt
@@ -1,21 +1,18 @@
 package br.senai.sp.jandira.projetoprofile.screens
 
-import android.provider.ContactsContract.Profile
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Groups
-import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.PersonAdd
 import androidx.compose.material.icons.filled.Phone
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.filled.Whatsapp
@@ -24,11 +21,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -91,7 +86,7 @@ fun ProfileScreen() {
 
                     Spacer(modifier = Modifier.height(20.dp))
 
-                    // Ações representadas por ícones estáticos
+
                     Row(
                         horizontalArrangement = Arrangement.SpaceEvenly,
                         modifier = Modifier
